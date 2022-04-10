25e9848849507c2dc16a244b32bb5c19208f82cb
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-04-11 01:09:45 +0530

    add authorization

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,13 +1,10 @@
 import Header from './Header';
 
-const AppContainer = (props: {
-  currentUser: User;
-  children: React.ReactNode;
-}) => {
+const AppContainer = (props: { children: React.ReactNode }) => {
   return (
     <div className='flex flex-col h-screen bg-gray-100 justify-start p-8 overflow-y-scroll'>
       <div className='p-5 mx-auto my-auto bg-white shadow-lg rounded-xl w-2/5'>
-        <Header currentUser={props.currentUser} />
+        <Header />
         {props.children}
       </div>
     </div>
